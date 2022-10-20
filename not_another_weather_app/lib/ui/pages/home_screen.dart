28eee362d012dfaa387932f503a3289c75c36198
FerBueroTrebino <ferbuerotrebino@gmail.com>
2022-10-20 16:12:35 +0200

    First implementation of Home Screen

--- not_another_weather_app/lib/ui/pages/home_screen.dart
+++ not_another_weather_app/lib/ui/pages/home_screen.dart
@@ -7,6 +7,46 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Not Another Weather App')),
+      body: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'City Name',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                Text(
+                  '28',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              SizedBox(width: 20.0),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Lower'),
+                ),
+              ),
+              SizedBox(width: 20.0),
+              Expanded(
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Higher'),
+                ),
+              ),
+              SizedBox(width: 20.0),
+            ],
+          ),
+          SizedBox(height: 20.0),
+        ],
+      ),
     );
   }
 }
