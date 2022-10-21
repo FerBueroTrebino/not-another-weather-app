1c73a0ac7f17e63f13f24b4e03643f0823fb9c62
FerBueroTrebino <ferbuerotrebino@gmail.com>
2022-10-21 15:35:19 +0200

    Handling errors on API service

--- not_another_weather_app/lib/services/weather_api.dart
+++ not_another_weather_app/lib/services/weather_api.dart
@@ -2,22 +2,32 @@ import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
-import 'package:not_another_weather_app/models/api_failure.dart';
+import 'package:not_another_weather_app/constants.dart';
 import 'package:not_another_weather_app/models/weather.dart';
+import 'package:not_another_weather_app/models/api_failure.dart';
 
 class WeatherApi {
-  // TODO: poner en constants
-  final String apiKey = '2bae4d5b768bdd1a675211939eaeb313';
-
-  final String apiUrl = 'https://api.openweathermap.org/data/2.5/';
-
-  Future<Weather> getWeather(Coord gpdCoords) async {
+  Future<Weather> getWeather(Coord gpdCoords, String unit) async {
     try {
-      var response = await http.get(Uri.parse(
-          "$apiUrl weather?lat=${gpdCoords.lat}lon=${gpdCoords.lon}&appid=$apiKey"));
+      http.Response response = await http.get(
+        Uri.parse(
+          "${apiUrl}weather?lat=${gpdCoords.lat}&lon=${gpdCoords.lon}&units=$unit&appid=$apiKey",
+        ),
+      );
 
-      Weather userInfoResult = weatherFromJson(response.body);
-      return userInfoResult;
+      if (response.statusCode == 200) {
+        try {
+          Weather userInfoResult = weatherFromJson(response.body);
+          return userInfoResult;
+        } catch (e) {
+          throw ApiFailure(
+              message: 'Bad response JSON format',
+              errorCode: response.statusCode);
+        }
+      } else {
+        throw ApiFailure(
+            message: 'Bad request to API', errorCode: response.statusCode);
+      }
     } on SocketException {
       throw ApiFailure(message: 'No internet conection');
     } on HttpException {
