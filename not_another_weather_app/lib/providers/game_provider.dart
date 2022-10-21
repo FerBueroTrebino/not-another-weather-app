127623c05bc58a3c39423a7870409b1284af044b
FerBueroTrebino <ferbuerotrebino@gmail.com>
2022-10-21 16:13:13 +0200

    Add states (initial, loading, loaded) and generate random coords private function

--- not_another_weather_app/lib/providers/game_provider.dart
+++ not_another_weather_app/lib/providers/game_provider.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'package:not_another_weather_app/constants.dart';
 import 'package:not_another_weather_app/models/api_failure.dart';
 import 'package:not_another_weather_app/models/weather.dart';
 import 'package:not_another_weather_app/services/weather_api.dart';
+
+enum NotifierState { initial, loading, loaded }
 
 class GameProvider extends ChangeNotifier {
   GameProvider() {
@@ -26,15 +30,39 @@ class GameProvider extends ChangeNotifier {
     notifyListeners();
   }
 
+  Coord _getRandomCoords() {
+    final random = Random();
+    double randomLat = -90 + random.nextDouble() * 90 * 2;
+    double randomLng = -180 + random.nextDouble() * 180 * 2;
+    return Coord(lat: randomLat, lon: randomLng);
+  }
+
   getCityWeather() async {
-    Coord sampleCoord = Coord(lat: 41.39182, lon: 2.1719681);
+    _setState(NotifierState.loading);
+    // Coord sampleCoord = Coord(lat: 41.39182, lon: 2.1719681);
 
     try {
       Weather weather = await weatherApi.getWeather(
-          sampleCoord, Units.metric.toShortString());
-      setWeather(weather);
+          _getRandomCoords(), Units.metric.toShortString());
+
+      // If the random location doestn have a city name asociated, will get a new location.
+      if (weather.name == '') {
+        getCityWeather();
+      } else {
+        setWeather(weather);
+        _setState(NotifierState.loaded);
+      }
     } on ApiFailure catch (f) {
+      _setState(NotifierState.loaded);
       setFailure(f);
     }
+  }
+
+  NotifierState _state = NotifierState.initial;
+  NotifierState get state => _state;
+
+  void _setState(NotifierState state) {
+    _state = state;
+    notifyListeners();
   }
 }
